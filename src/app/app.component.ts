07ace96009d00983bbd666fa27c7ea4d79c3c7c7
Jason Hatfield <jasonmhatfield@pm.me>
2023-05-11 19:58:28 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'musicapp';
+  title = 'My Music Collection';
+  version = '1.0';
+
+  constructor(private router: Router) {}
+
+  public displayVersion() {
+    alert(this.title + ' Version: ' + this.version);
+  }
+
+  public displayArtistList() {
+    this.router.navigate(['list-artists'], {
+      queryParams: { data: new Date() },
+    });
+  }
 }
