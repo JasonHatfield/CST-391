42365636a9417c30b43c4017ef829034129d9c73
Jason Hatfield <jasonmhatfield@pm.me>
2023-05-12 08:27:11 -0400

    Added all functions except for edit.

--- src/app/list-albums/list-albums.component.ts
+++ src/app/list-albums/list-albums.component.ts
@@ -1,30 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MusicServiceService } from '../service/music-service.service';
+import { MusicServiceService} from '../service/music-service.service';
 import { Artist } from '../models/Artist';
 import { Album } from '../models/Album';
 
 @Component({
   selector: 'app-list-albums',
   templateUrl: './list-albums.component.html',
-  styleUrls: ['./list-albums.component.css'],
+  styleUrls: ['./list-albums.component.css']
 })
-export class ListAlbumsComponent implements OnInit {
+export class ListAlbumsComponent implements OnInit
+{
   @Input() artist: Artist;
-  albums: Album[] = [];
+  albums:Album[] = [];
   selectedAlbum: Album = null;
 
-  constructor(private service: MusicServiceService) {}
+  constructor(private service: MusicServiceService) { }
 
-  ngOnInit() {
-    console.log('Getting albums for artist:', this.artist.Name);
-    this.service.getAlbums(this.artist.Name, (albums: Album[]) => {
-      console.log('Fetched albums:', albums);
-      this.albums = albums;
+  ngOnInit()
+  {
+    this.service.getAlbums( this.artist.Name, (albums:Album[]) =>
+    {
+        this.albums = albums;
     });
+
   }
 
-  public onSelectAlbum(album: Album) {
-    console.log('Selected Album of ' + album.Title);
+  public onSelectAlbum(album: Album)
+  {
+    console.log("Selected Album of " + album.Title);
     this.selectedAlbum = album;
   }
 }
