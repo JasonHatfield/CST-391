02e4e1b2fb419ac20a9336b008307fe10a32f2a0
Jason Hatfield <jasonmhatfield@pm.me>
2023-05-12 15:23:52 -0400

    removed the home feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'movie-list', component: MovieListComponent },
   { path: 'add-movie', component: AddMovieComponent, pathMatch: 'full' },
   { path: 'movie-details/:id', component: MovieDetailsComponent },
