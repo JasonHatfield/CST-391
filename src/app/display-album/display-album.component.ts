742932821483b573e6d9dbcdda579ac2319a7d5b
Jason Hatfield <jasonmhatfield@pm.me>
2023-05-11 20:11:33 -0400

    Completed - Testing/Troubleshooting

--- src/app/display-album/display-album.component.ts
+++ src/app/display-album/display-album.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Album } from '../models/Album';
 
 @Component({
   selector: 'app-display-album',
   templateUrl: './display-album.component.html',
-  styleUrls: ['./display-album.component.css']
+  styleUrls: ['./display-album.component.css'],
 })
-export class DisplayAlbumComponent {
+export class DisplayAlbumComponent implements OnInit {
+  @Input() album: Album | null = null;
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- src/app/service/music-service.service.ts
+++ src/app/service/music-service.service.ts
@@ -8,64 +8,98 @@ import { Track } from '../models/Track';
 
 @Injectable({ providedIn: 'root' })
 export class MusicServiceService {
-	private readonly artists: Artist[] = [];
-	private readonly albums: Album[] = [];
+  private readonly artists: Artist[] = [];
+  private readonly albums: Album[] = [];
 
-	constructor() {
-		this.createArtists();
-		this.createAlbums();
-	}
+  constructor() {
+    this.createArtists();
+    this.createAlbums();
+  }
 
-	private createArtists(): void {
-		this.artists.push(new Artist(0, 'The Beatles'));
-	}
+  private createArtists(): void {
+    this.artists.push(new Artist(0, 'The Beatles'));
+  }
 
-	private createAlbums(): void {
-		exampledata.forEach((data: any) => {
-			if (data.artist === 'The Beatles') {
-				const tracks = data.tracks.map((trackData: any) => new Track(trackData.id, trackData.number, trackData.title, trackData.lyrics, trackData.video));
-				const album = new Album(data.id, data.title, data.artist, data.description, data.year, data.image, tracks);
-				this.albums.push(album);
-			}
-		});
-	}
+  private createAlbums(): void {
+    exampledata.forEach((data: any) => {
+      if (data.artist === 'The Beatles') {
+        const tracks = data.tracks.map(
+          (trackData: any) =>
+            new Track(
+              trackData.id,
+              trackData.number,
+              trackData.title,
+              trackData.lyrics,
+              trackData.video
+            )
+        );
+        const album = new Album(
+          data.id,
+          data.title,
+          data.artist,
+          data.description,
+          data.year,
+          data.image,
+          tracks
+        );
+        this.albums.push(album);
+      }
+    });
+  }
 
-	public getArtists(): Artist[] {
-		return this.artists;
-	}
+  public getArtists(): Artist[] {
+    return this.artists;
+  }
 
-	public getAlbums(artist: string): Album[] {
-		return this.albums;
-	}
+  public getAlbums(artist: string): Album[] {
+    return this.albums;
+  }
 
-	public getAlbum(artist: string, id: number): Album | undefined {
-		const album = this.albums.find((a) => a.Artist === artist && a.Id === id);
+  public getAlbum(artist: string, id: number): Album | undefined {
+    const album = this.albums.find((a) => a.Artist === artist && a.Id === id);
 
-		if (album) {
-			const tracks = album.Tracks.map((track) => new Track(track.Id, track.Number, track.Title, track.Lyrics, track.Video));
-			return new Album(album.Id, album.Title, album.Artist, album.Description, album.Year, album.Image, tracks);
-		}
+    if (album) {
+      const tracks = album.Tracks.map(
+        (track) =>
+          new Track(
+            track.Id,
+            track.Number,
+            track.Title,
+            track.Lyrics,
+            track.Video
+          )
+      );
+      return new Album(
+        album.Id,
+        album.Title,
+        album.Artist,
+        album.Description,
+        album.Year,
+        album.Image,
+        tracks
+      );
+    }
 
-		return undefined;
-	}
+    return undefined;
+  }
 
-	public createAlbum(album: Album): void {
-		this.albums.push(album);
-	}
+  public createAlbum(album: Album): void {
+    this.albums.push(album);
+  }
 
-	public updateAlbum(album: Album): void {
-		const index = this.albums.findIndex((a) => a.Id === album.Id);
+  public updateAlbum(album: Album): void {
+    const index = this.albums.findIndex((a) => a.Id === album.Id);
 
-		if (index !== -1) {
-			this.albums.splice(index, 1, album);
-		}
-	}
+    if (index !== -1) {
+      this.albums.splice(index, 1, album);
+    }
+  }
 
-	public deleteAlbum(id: number, artist: string): void {
-		const index = this.albums.findIndex((a) => a.Id === id);
+  public deleteAlbum(id: number, artist: string): void {
+    const index = this.albums.findIndex((a) => a.Id === id);
 
-		if (index !== -1) {
-			this.albums.splice(index, 1);
-		}
-	}
+    if (index !== -1) {
+      this.albums.splice(index, 1);
+    }
+  }
 }
