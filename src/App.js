008ece0edf15c9431d27ba76191bb78b1eac4e49
Jason Hatfield <jasonmhatfield@pm.me>
2023-05-31 05:59:34 -0400

    Blog app completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Post from './Post';
+import AddPost from './AddPost';
 
 function App() {
+  const [postList, setPostList] = useState([
+    {
+      postNumber: 0,
+      text: 'A short psychic broke out of jail. She was a small medium at large.',
+    },
+    {
+      postNumber: 1,
+      text: 'Second hardcoded post text',
+    },
+    {
+      postNumber: 2,
+      text: 'Third hardcoded post text',
+    },
+  ]);
+
+  const handleDeletePost = (id) => {
+    let updatedPostList = postList.filter((post) => post.postNumber !== id);
+  
+    updatedPostList = updatedPostList.map((post) =>
+      post.postNumber > id
+        ? { ...post, postNumber: post.postNumber - 1 }
+        : post
+    );
+  
+    setPostList(updatedPostList);
+  };
+  
+
+  const handleAddPost = (newText) => {
+    let newPostNumber = 0;
+  
+    // Generate the smallest unused id
+    for (let i = 0; ; i++) {
+      if (!postList.some((post) => post.postNumber === i)) {
+        newPostNumber = i;
+        break;
+      }
+    }
+  
+    let newPost = {
+      postNumber: newPostNumber,
+      text: newText,
+    };
+  
+    setPostList((postList) => [...postList, newPost]);
+  }; 
+
+  const posts = postList
+    .sort((a, b) => a.postNumber - b.postNumber)
+    .map((post) => (
+      <Post key={post.postNumber} text={post.text} id={post.postNumber} onDelete={handleDeletePost} />
+    ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {posts}
+      <AddPost onAdd={handleAddPost} />
     </div>
   );
 }
